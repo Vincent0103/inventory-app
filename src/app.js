ae0e49beb23ca799570ff711ace4f4b052eb27f7
Vincent0103 <vincentkanagasundaram@gmail.com>
2025-06-27 11:16:08 +0200

    Allow integration with tailwindcss

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Serve static files (for CSS, JS, images)
-app.use(express.static(path.join(__dirname, "assets")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", usersRouter);
 app.use("/:itemName", itemRouter);
