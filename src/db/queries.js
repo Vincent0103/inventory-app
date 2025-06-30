4ddd72cb07b374739882c0ca7e0cc56255159742
Vincent0103 <vincentkanagasundaram@gmail.com>
2025-06-30 22:09:30 +0200

    Cover every data retrieval and manipulation via database

--- src/db/queries.js
+++ src/db/queries.js
@@ -1,5 +1,102 @@
 import pool from "./pool";
 
+const getFilters = async () => {
+  const categoryRows = (
+    await pool.query("SELECT nameCategory, formNameCategory FROM CATEGORY")
+  ).rows;
+  const categories = await Promise.all(
+    (categoryRows ?? []).map((category) => ({
+      name: category.namecategory,
+      formName: category.formnamecategory,
+    })),
+  );
+
+  const animalRows = (
+    await pool.query("SELECT nameAnimal, formNameAnimal FROM ANIMAL")
+  ).rows;
+  const animals = await Promise.all(
+    (animalRows ?? []).map((animal) => ({
+      name: animal.nameanimal,
+      formName: animal.formnameanimal,
+    })),
+  );
+
+  return [
+    {
+      filterName: "categories",
+      filterChildName: "category",
+      list: categories,
+    },
+    {
+      filterName: "animals",
+      filterChildName: "animal",
+      list: animals,
+    },
+  ];
+};
+
+const getCategories = async (idPlushy) => {
+  const categoryRows = (
+    await pool.query(
+      "SELECT idCategory FROM CATEGORYPLUSHY WHERE idPlushy = $1",
+      [idPlushy],
+    )
+  ).rows;
+
+  const categories = await Promise.all(
+    (categoryRows ?? []).map(async (row) => {
+      const {
+        namecategory,
+        formnamecategory,
+        bgcolorcategory,
+        bordercolorcategory,
+        textwhite,
+      } = (
+        await pool.query(
+          "SELECT nameCategory, formNameCategory, bgColorCategory, borderColorCategory, textWhite FROM CATEGORY WHERE idCategory = $1",
+          [row.idcategory],
+        )
+      ).rows[0];
+
+      return {
+        name: namecategory,
+        formName: formnamecategory,
+        bgColor: bgcolorcategory,
+        borderColor: bordercolorcategory,
+        textColor: textwhite ? "#FFFFFF" : "#000000",
+      };
+    }),
+  );
+
+  return categories;
+};
+
+const getItems = async () => {
+  const plushyRows = (
+    await pool.query(
+      "SELECT idPlushy, imgSrcPlushy, imgAltPlushy, urlNamePlushy, namePlushy, pricePlushy, stocksLeftPlushy FROM PLUSHY",
+    )
+  ).rows;
+
+  const items = await Promise.all(
+    (plushyRows ?? []).map(async (plushyRow) => {
+      const categories = await getCategories(plushyRow.idplushy);
+
+      return {
+        imgSrc: plushyRow.imgsrcplushy,
+        imgAlt: plushyRow.imgaltplushy,
+        urlName: plushyRow.urlnameplushy,
+        name: plushyRow.nameplushy,
+        price: plushyRow.priceplushy,
+        categories,
+        amountAvailable: plushyRow.stocksleftplushy,
+      };
+    }),
+  );
+
+  return items;
+};
+
 const getItem = async (itemUrlName) => {
   const plushyRow = (
     await pool.query("SELECT * FROM PLUSHY WHERE urlNamePlushy = $1", [
@@ -50,36 +147,7 @@ const getItem = async (itemUrlName) => {
     }),
   );
 
-  const categoryRows = (
-    await pool.query(
-      "SELECT idCategory FROM CATEGORYPLUSHY WHERE idPlushy = $1",
-      [plushyRow.idplushy],
-    )
-  ).rows;
-
-  const categories = await Promise.all(
-    (categoryRows ?? []).map(async (row) => {
-      const {
-        namecategory,
-        formnamecategory,
-        bgcolorcategory,
-        bordercolorcategory,
-        textwhite,
-      } = (
-        await pool.query(
-          "SELECT nameCategory, formNameCategory, bgColorCategory, borderColorCategory, textWhite FROM CATEGORY WHERE idCategory = $1",
-          [row.idcategory],
-        )
-      ).rows[0];
-      return {
-        name: namecategory,
-        formName: formnamecategory,
-        bgColor: bgcolorcategory,
-        borderColor: bordercolorcategory,
-        textColor: textwhite ? "#FFFFFF" : "#000000",
-      };
-    }),
-  );
+  const categories = await getCategories(plushyRow.idPlushy);
 
   const item = {
     id: plushyRow.idplushy,
@@ -101,4 +169,4 @@ const getItem = async (itemUrlName) => {
   return item;
 };
 
-export { getItem };
+export { getItem, getItems, getFilters };
