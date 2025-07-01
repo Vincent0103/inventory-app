d8261797c4b90b115fd62c0475a174758cb32153
Vincent0103 <vincentkanagasundaram@gmail.com>
2025-07-01 22:35:26 +0200

    Add name, creation-date and img-url first validation checks

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,5 @@
+import { body, validationResult } from "express-validator";
+
 const usersController = (() => {
   const homepageGet = (req, res) => {
     res.render("index");
@@ -6,7 +8,37 @@ const usersController = (() => {
   const createItemGet = (req, res) => {
     res.render("createPlushy");
   };
-  const createItemPost = [null, (req, res) => {}];
+
+  const alphaErr = "must only contain letters.";
+  const lengthErr = "must be between 1 and 255 characters";
+  const dateErr = "must be in the format DD/MM/YYYY";
+
+  const validatePlushy = [
+    body("name")
+      .trim()
+      .isAlpha()
+      .withMessage(`Name ${alphaErr}`)
+      .isLength({ min: 1, max: 255 })
+      .withMessage(`Name ${lengthErr}`),
+    body("creation-date")
+      .trim()
+      .isDate()
+      .withMessage(`Creation date ${dateErr}`),
+  ];
+
+  const createItemPost = [
+    validatePlushy,
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render("createPlushy", {
+          errors: errors.array(),
+        });
+      }
+
+      res.redirect("/inventory");
+    },
+  ];
 
   return { homepageGet, createItemGet, createItemPost };
 })();
