c930fde34bbf2945f8c71e8198d763af0c835525
Vincent0103 <vincentkanagasundaram@gmail.com>
2025-07-06 15:54:50 +0200

    Fix routing issues

--- src/routes/editRouter.js
+++ src/routes/editRouter.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 import editController from "../controllers/editController";
 
 const editRouter = Router();
-editRouter.get("/:plushySlug", editController.plushyEditGet);
-editRouter.post("/:plushySlug", editController.plushyEditPost);
-editRouter.get("/:categorySlug", editController.categoryEditGet);
-editRouter.post("/:categorySlug", editController.categoryEditPost);
+editRouter.get("/plushy/:plushySlug", editController.plushyEditGet);
+editRouter.post("/plushy/:plushySlug", editController.plushyEditPost);
+editRouter.get("/category/:categorySlug", editController.categoryEditGet);
+editRouter.post("/category:categorySlug", editController.categoryEditPost);
 
 export default editRouter;
